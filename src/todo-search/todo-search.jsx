488ad5bb013aa1d5bd5698b909cd3335b0ce2be9
David <davidgranados73@gmail.com>
2022-03-15 08:14:26 -0500

    manejo de estado

--- src/todo-search/todo-search.jsx
+++ src/todo-search/todo-search.jsx
@@ -1,13 +1,16 @@
+import { useState } from 'react'
 import PropTypes from 'prop-types'
 
 import styles from './todo-search.module.css'
 
 const TodoSearch = ({ placeholder = 'cebolla' }) => {
+  const [query, setQuery] = useState('')
   const handleChange = (event) => {
     console.log(
       '🚀 ~ file: todo-search.jsx ~ line 8 ~ handleChange ~ event.target.value',
       event.target.value
     )
+    setQuery(event.target.value)
   }
   return (
     <input
@@ -15,6 +18,7 @@ const TodoSearch = ({ placeholder = 'cebolla' }) => {
       className={styles['todo-search']}
       placeholder={placeholder}
       onChange={handleChange}
+      value={query}
     />
   )
 }
